188031727e28bd0d58eb374c5199a997118efcda
Purnaman Rai <purnamaanrai7@gmail.com>
2025-08-19 14:14:38 +0545

    Initial Commit

---
+++ ProjectOverviews/ProjectOverviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectOverviewsApp.swift
+//  ProjectOverviews
+//
+//  Created by Purnaman Rai (College) on 19/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ProjectOverviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
