9c3bbd4977685943eca8fcdb5fd9e88f260cb55a
Purnaman Rai <purnamaanrai7@gmail.com>
2025-08-19 15:51:39 +0545

    practice datepicker

--- ProjectOverviews/BetterRest.swift
+++ ProjectOverviews/BetterRest.swift
@@ -10,8 +10,18 @@ import SwiftUI
 struct BetterRest: View {
     @State private var sleepAmount = 8.0
     
+    @State private var wakeUp = Date.now
+    @State private var tomorrow = Date.now.addingTimeInterval(86400)
+    
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+        VStack {
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            
+            DatePicker("Please select a date", selection: $wakeUp, in: Date.now..., displayedComponents: .date)
+            DatePicker("Please select a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                .labelsHidden()
+        }
+        .padding()
     }
 }
 
